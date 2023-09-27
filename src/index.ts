ce2aa8bff8a46ad533acbc20404d3cf6eaca51a0
m2lovric <m2lovric@gmail.com>
2023-09-27 19:01:17 +0200

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import { subforumsRoute, userRoute, postsRoute } from './routes/routes';
-import { jwtAuth } from './middleware/jwtAuth';
+import { getCookie, jwtAuth } from './middleware/jwtAuth';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    exposedHeaders: ['Authorization'],
+  })
+);
+
 app.use(cookieParser());
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.on('finish', () => {
@@ -22,7 +34,7 @@ app.get('/', async (req, res) => {
 
 app.use('/api', subforumsRoute);
 app.use('/api', userRoute);
-app.use('/api', jwtAuth, postsRoute);
+app.use('/api', getCookie, postsRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`);
